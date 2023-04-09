62c64e1b790e686d2ad1abc377a4f9fe16ca40d0
elena_vagner <dipnoe@gmail.com>
2023-04-09 13:41:39 +0300

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,22 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+from src.item import Item
+
+
+@pytest.fixture
+def item():
+    return Item("Смартфон", 10_000, 20)
+
+
+def test_item_init(item):
+    assert item.name == "Смартфон"
+    assert item.price == 10_000
+    assert item.quantity == 20
+
+
+def test_calculate_total_price(item):
+    assert item.calculate_total_price() == 200_000
+
+
+def test_apply_discount(item):
+    assert item.apply_discount() is None
