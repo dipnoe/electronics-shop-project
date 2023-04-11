bc6d2cc20c05d1eefeebd9673d9ba7dba3c5c468
elena_vagner <dipnoe@gmail.com>
2023-04-12 01:55:04 +0300

    add new tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -9,6 +9,7 @@ def item():
 
 
 def test_item_init(item):
+    assert item.__class__ == Item
     assert item.name == "Смартфон"
     assert item.price == 10_000
     assert item.quantity == 20
@@ -20,3 +21,20 @@ def test_calculate_total_price(item):
 
 def test_apply_discount(item):
     assert item.apply_discount() is None
+
+
+def test_name_length(item):
+    item.name = 'Наушники'
+    assert item.name == 'Наушники'
+    with pytest.raises(Exception):
+        item.name = 'Отвратительно длинное название'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('2') == 2
+
+
+def test_instantiate_from_csv():
+    Item.all = []
+    Item.instantiate_from_csv()
+    assert len(Item.all) == 5
