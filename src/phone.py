47766504b990f0e96fb49f6ea8032adcbf487ebb
elena_vagner <dipnoe@gmail.com>
2023-04-19 21:03:52 +0300

    delete the comment

--- src/phone.py
+++ src/phone.py
@@ -17,8 +17,6 @@ class Phone(Item):
         else:
             self.__number_of_sim = number_of_sim_
 
-    # def __repr__(self):
-    #     return f'{super().__repr__()}, {self.number_of_sim}'
     def __repr__(self) -> str:
         return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
@@ -30,4 +28,3 @@ class Phone(Item):
             return self.quantity + other.quantity
         else:
             raise ValueError('Можно складывать только количество Item и Phone')
-
