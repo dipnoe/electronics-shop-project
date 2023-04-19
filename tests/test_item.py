6135a9e452401014b12d3cadb8725a376d74ecb9
elena_vagner <dipnoe@gmail.com>
2023-04-19 21:04:27 +0300

    add tests for class Phone and new fixture phone

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,11 +1,17 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 import pytest
 from src.item import Item
+from src.phone import Phone
 
 
 @pytest.fixture
 def item():
     return Item("Смартфон", 10_000, 20)
+
+
+@pytest.fixture
+def phone():
+    return Phone("iPhone 14", 120_000, 5, 2)
 
 
 def test_item_init(item):
@@ -46,3 +52,23 @@ def test_repr(item):
 
 def test_str(item):
     assert str(item) == 'Смартфон'
+
+
+def test_add(phone, item):
+    assert item + phone == 25
+    assert item + item == 40
+    assert phone + phone == 10
+    assert phone + item == 25
+
+
+def test_number_of_sim(phone):
+    assert phone.number_of_sim == 2
+    phone.number_of_sim = 4
+    assert phone.number_of_sim == 4
+    with pytest.raises(ValueError):
+        phone.number_of_sim = 0
+        phone.number_of_sim = 'u'
+
+
+def test_repr_phone(phone):
+    assert repr(phone) == "Phone('iPhone 14', 120000, 5, 2)"
